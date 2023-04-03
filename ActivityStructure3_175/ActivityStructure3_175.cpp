59ac869998a23c2182c15a3518d7bb670bf44513
NaufalSatriaa <satriana0017@gmail.com>
2023-04-03 15:21:26 +0700

    membuat looping tentang input mahasiswa

--- ActivityStructure3_175/ActivityStructure3_175.cpp
+++ ActivityStructure3_175/ActivityStructure3_175.cpp
@@ -15,4 +15,20 @@ struct Mahasiswa {
 
 int main() {
 	Mahasiswa mhs[3];
+
+	for (int i = 0; i < 3; i++0) {
+		cout << "masukkan NIM : ";
+		cin.getline(mhs[i].NIM, 12);
+		cout << "masukkan Nama : ";
+		cin.getline(mhs[i].Nama, 12);
+		cout << "masukkan Alamat" << endl;
+		cout << "masukkan desa : ";
+		cin.getline(mhs[i].Alamat.Desa, 12);
+		cout << "masukkan Kota : ";
+		cin.getline(mhs[i].Alamat.Kota, 12);
+		cout << "masukkan umur : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+	}
+
 }
