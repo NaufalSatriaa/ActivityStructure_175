8692a983c201c09d3aa427bdefa9333b516b28a6
NaufalSatriaa <satriana0017@gmail.com>
2023-04-03 14:35:33 +0700

    membuat input mhs2

--- ActivityStructure_175/ActivityStructure_175.cpp
+++ ActivityStructure_175/ActivityStructure_175.cpp
@@ -15,4 +15,13 @@ int main() {
 	mhs1.Nama = "Natasya";
 	mhs1.Alamat = "Semarang";
 	mhs1.umur = 20;
+
+	cout << "Masukkan NIM : ";
+	cin >> mhs2.NIM;
+	cout << "Masukkan Nama : ";
+	cin >> mhs2.Nama;
+	cout << "Masukkan Alamat : ";
+	cin >> mhs2.Alamat;
+	cout << "Masukkan Umur : ";
+	cin >> mhs2.umur;
 }
