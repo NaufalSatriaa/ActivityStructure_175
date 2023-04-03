cf2cead1fa48c1c4ca87cfd7df77627529f16eed
NaufalSatriaa <satriana0017@gmail.com>
2023-04-03 14:56:33 +0700

    membuat input

--- ActivityStructure2_175/ActivityStructure2_175.cpp
+++ ActivityStructure2_175/ActivityStructure2_175.cpp
@@ -15,4 +15,16 @@ struct Mahasiswa {
 
 int main() {
 	Mahasiswa mhs;
+
+	cout << "Masukkan NIM : ";
+	cin >> mhs.NIM;
+	cout << "Masukkan Nama : ";
+	cin >> mhs.Nama;
+	cout << "Mauskkan Alamat" << endl;
+	cout << "Masukkan Desa : ";
+	cin >> mhs.Alamat.Desa;
+	cout << "Masukkan Kota : ";
+	cin >> mhs.Alamat.Kota;
+	cout << "Masukkan Umur : ";
+	cin >> mhs.umur;
 }
